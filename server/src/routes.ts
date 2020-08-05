bcbe56ef03160c6ab56d21121efb6cf1c41a54de
Felipe Frantz Zanini <devffzanini@gmail.com>
2020-08-05 18:05:14 -0300

    🐛 fix: insert and list in classes/connections
    now is working without errors

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsController = new ConnectionController();
 
-routes.post('/classes', (request, response) => {
-    const data  = request.body;
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
-    console.log(data);
-    
-    return response.send();
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
