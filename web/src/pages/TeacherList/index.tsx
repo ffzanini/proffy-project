a0004445b29ef536ac45dac84f335d944ba24891
Felipe Frantz Zanini <devffzanini@gmail.com>
2020-08-13 21:38:15 -0300

    ⚡️ improved: teacher list

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -57,7 +57,7 @@ function TeacherList() {
                         onChange = {(e) => {setWeekDay(e.target.value)}}
                         options={[
                             {value: '0', label: 'Segunda-feira'},
-                            {value: '1', label: 'Terça-feiora'},
+                            {value: '1', label: 'Terça-feira'},
                             {value: '2', label: 'Quarta-feira'},
                             {value: '3', label: 'Quinta-feira'},
                             {value: '4', label: 'Sexta-feira'},
